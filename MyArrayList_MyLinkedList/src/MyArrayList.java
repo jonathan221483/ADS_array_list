a888c4b9a905dd347e552ac7c9a6fccd183d0d6c
jonathan221483 <captainberkut@gmail.com>
2023-04-19 11:47:10 +0600

    ads_attempt1

---
+++ MyArrayList_MyLinkedList/src/MyArrayList.java
@@ -0,0 +1,62 @@
+import java.util.Arrays;
+public class MyArrayList implements MyList {
+
+    private int size;
+    private Object[] data = new Object[3];
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        return false;
+    }
+
+    @Override
+    public void add(Object item) {
+        size++;
+        data = Arrays.copyOf(data, data.length+1);
+        data[size]=item;
+    }
+
+    @Override
+    public void add(Object item, int index) {
+
+    }
+
+    @Override
+    public boolean remove(Object item) {
+        return false;
+    }
+
+    @Override
+    public Object remove(int index) {
+        return null;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public Object get(int index) {
+        return null;
+    }
+
+    @Override
+    public int indexOf(Object o) {
+        return 0;
+    }
+
+    @Override
+    public int lastIndexOf(Object o) {
+        return 0;
+    }
+
+    @Override
+    public void sort() {
+
+    }
+}
