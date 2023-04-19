9246cc6b2ec3b1bf523589a701da39aeac5fe9ab
jonathan221483 <captainberkut@gmail.com>
2023-04-19 23:19:58 +0600

    nodes methods

--- MyArrayList_MyLinkedList/src/MyLinkedList.java
+++ MyArrayList_MyLinkedList/src/MyLinkedList.java
@@ -14,7 +14,24 @@ public class MyLinkedList<T> implements MyList<T>{
         public Node(E data){
             this.data = data;
         }
-
+        public E getData() {
+            return data;
+        }
+        public void setData(E data) {
+            this.data = data;
+        }
+        public Node<E> getNext() {
+            return next;
+        }
+        public void setNext(Node<E> next) {
+            this.next = next;
+        }
+        public Node<E> getPrevious() {
+            return previous;
+        }
+        public void setPrevious(Node<E> previous) {
+            this.previous = previous;
+        }
     }
 
 
